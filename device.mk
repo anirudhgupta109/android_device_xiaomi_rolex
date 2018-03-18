9bef3210686d51a6c4c6b4d13094957d5f0e24bf
Bruno Martins <bgcngm@gmail.com>
2018-03-18 21:20:20 +0000

    rolex: Remove non-existing HAL services
    Change-Id: I26da013dfa359ec8f184a20bc881558be0144b4a

--- device.mk
+++ device.mk
@@ -78,9 +78,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service
+    android.hardware.soundtrigger@2.0-impl
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
